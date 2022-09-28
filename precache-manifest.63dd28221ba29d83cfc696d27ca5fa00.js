0ea547d556801684ab2d438def8bfcfb994988ea
TuanDungLuong <luongtuandung181209@outlook.com>
2022-09-28 09:34:10 +0700

    Updates

---
+++ precache-manifest.63dd28221ba29d83cfc696d27ca5fa00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970e357532d20e8052dd365f985bb8da",
+    "url": "/chat-website-demo-ngon/index.html"
+  },
+  {
+    "revision": "39debb8bac818c263803",
+    "url": "/chat-website-demo-ngon/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "df3b3d8d255b86369016",
+    "url": "/chat-website-demo-ngon/static/js/2.0686d72b.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/chat-website-demo-ngon/static/js/2.0686d72b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39debb8bac818c263803",
+    "url": "/chat-website-demo-ngon/static/js/main.a2e81fc1.chunk.js"
+  },
+  {
+    "revision": "4ed0f263a4b595df2d6e",
+    "url": "/chat-website-demo-ngon/static/js/runtime-main.3256c19c.js"
+  }
+]);
